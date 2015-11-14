10abb5de14b72fdc998068a499ea1084e1a29289
Blaz Orazem <blaz@orazem.info>
2015-11-14 21:42:26 +0100

    Routes moved to service provider.

--- app/Http/Controllers/Auth/ProfileController.php
+++ app/Http/Controllers/Auth/ProfileController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
 use App\Utils\AppMailer;
 use Illuminate\Http\Request;
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,8 +24,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(Router $router)
     {
-        //
-
         parent::boot($router);
     }
 
@@ -38,6 +36,37 @@ class RouteServiceProvider extends ServiceProvider
     public function map(Router $router)
     {
         $router->group(['namespace' => $this->namespace], function ($router) {
+
+            $router->group(['namespace' => 'Auth', 'prefix' => 'auth'], function () {
+
+                // Authentication routes
+                $this->get('login',       'AuthController@getLogin')->name('login');
+                $this->post('login',      'AuthController@postLogin')->name('login_action');
+                $this->get('auth/logout', 'AuthController@getLogout')->name('logout');
+
+                // Registration routes
+                $this->get('register',                'AuthController@getRegister')->name('register');
+                $this->post('register',               'AuthController@postRegister')->name('register_action');
+                $this->get('register/verify/{token}', 'AuthController@verifyEmail');
+
+                // Social authentication routes
+                $this->get('social/{provider?}', 'SocialAuthController@getLogin')->name('login_social');
+
+                // Password reset link request routes
+                $this->get('password/email',  'PasswordController@getEmail')->name('password_forget');
+                $this->post('password/email', 'PasswordController@postEmail')->name('password_send');
+
+                // User profile routes
+                $this->get('profile',           'ProfileController@index')->name('profile');
+                $this->post('profile/update',   'ProfileController@updateProfile')->name('profile_update');
+                $this->post('profile/password', 'ProfileController@updatePassword')->name('password_update');
+
+            });
+
+            // Password reset routes
+            $router->get('password/reset/{token}', 'Auth\PasswordController@getReset');
+            $router->post('password/reset',        'Auth\PasswordController@postReset')->name('password_reset');
+
             require app_path('Http/routes.php');
         });
     }
