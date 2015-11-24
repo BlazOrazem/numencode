a95769716f070aaca2644911a50241461acc50ef
Blaž Oražem <blaz@orazem.info>
2015-11-24 18:03:25 +0100

    PSR-2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,19 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-	/**
-	 * Admin namespace title.
-	 *
-	 * @var string
-	 */
-	protected $adminNamespace = 'admin';
+    /**
+     * Admin namespace title.
+     *
+     * @var string
+     */
+    protected $adminNamespace = 'admin';
 
-	/**
-	 * Cms namespace title.
-	 *
-	 * @var string
-	 */
-	protected $cmsNamespace = 'cms';
+    /**
+     * Cms namespace title.
+     *
+     * @var string
+     */
+    protected $cmsNamespace = 'cms';
 
     /**
      * Bootstrap any application services.
@@ -27,13 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		// Load views for modules
-		$this->loadViewsFrom($this->app->basePath() . '/modules/Admin/Resources/views', $this->adminNamespace);
-		$this->loadViewsFrom($this->app->basePath() . '/modules/Cms/Resources/views', $this->cmsNamespace);
+        // Load views for modules
+        $this->loadViewsFrom($this->app->basePath() . '/modules/Admin/Resources/views', $this->adminNamespace);
+        $this->loadViewsFrom($this->app->basePath() . '/modules/Cms/Resources/views', $this->cmsNamespace);
 
-		// Load languages for modules
-		$this->loadTranslationsFrom($this->app->basePath() . '/modules/Admin/Resources/lang', $this->adminNamespace);
-		$this->loadTranslationsFrom($this->app->basePath() . '/modules/Cms/Resources/lang', $this->cmsNamespace);
+        // Load languages for modules
+        $this->loadTranslationsFrom($this->app->basePath() . '/modules/Admin/Resources/lang', $this->adminNamespace);
+        $this->loadTranslationsFrom($this->app->basePath() . '/modules/Cms/Resources/lang', $this->cmsNamespace);
     }
 
     /**
