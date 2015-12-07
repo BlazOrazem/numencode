ddbfa51ac719d76d295c091df1f510c9e51548d5
Blaž Oražem <blaz.orazem@spletna-postaja.com>
2015-12-07 13:51:00 +0100

    PSR-2

--- app/Http/Controllers/Auth/AuthWithLoginThrottleController.php
+++ app/Http/Controllers/Auth/AuthWithLoginThrottleController.php
@@ -4,7 +4,7 @@ namespace Numencode\Http\Controllers\Auth;
 
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 
-class AuthPlusController extends AuthController
+class AuthWithLoginThrottleController extends AuthController
 {
     use ThrottlesLogins;
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -9,26 +9,26 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 
 class RouteServiceProvider extends ServiceProvider
 {
-	/**
-	 * This namespace is applied to the controller routes in your routes file.
-	 *
-	 * @var string
-	 */
-	protected $namespace = 'Numencode\Http\Controllers';
+    /**
+     * This namespace is applied to the controller routes in your routes file.
+     *
+     * @var string
+     */
+    protected $namespace = 'Numencode\Http\Controllers';
 
-	/**
-	 * This namespace is applied to the admin controller routes in your routes file.
-	 *
-	 * @var string
-	 */
-	protected $adminNamespace = '\Admin\Http\\';
+    /**
+     * This namespace is applied to the admin controller routes in your routes file.
+     *
+     * @var string
+     */
+    protected $adminNamespace = '\Admin\Http\\';
 
-	/**
-	 * This namespace is applied to the cms controller routes in your routes file.
-	 *
-	 * @var string
-	 */
-	protected $cmsNamespace = '\Cms\Http\\';
+    /**
+     * This namespace is applied to the cms controller routes in your routes file.
+     *
+     * @var string
+     */
+    protected $cmsNamespace = '\Cms\Http\\';
 
     /**
      * Authentication controller name.
@@ -37,116 +37,116 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected $authController;
 
-	/**
-	 * Define your route model bindings, pattern filters, etc.
-	 *
-	 * @param  \Illuminate\Routing\Router $router
-	 * @return void
-	 */
-	public function boot(Router $router)
-	{
-		parent::boot($router);
-	}
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @param  \Illuminate\Routing\Router $router
+     * @return void
+     */
+    public function boot(Router $router)
+    {
+        parent::boot($router);
+    }
 
-	/**
-	 * Define the routes for the application.
-	 *
-	 * @param  \Illuminate\Routing\Router $router
-	 * @return void
-	 */
-	public function map(Router $router)
-	{
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router $router
+     * @return void
+     */
+    public function map(Router $router)
+    {
         $this->authController = 'AuthController';
 
         if (config('login.throttle')) {
-            $this->authController = 'AuthPlusController';
+            $this->authController = 'AuthWithLoginThrottleController';
         }
 
-		$router->group(['namespace' => $this->namespace], function ($router) {
-			$this->publicRoutes($router);
+        $router->group(['namespace' => $this->namespace], function ($router) {
+            $this->publicRoutes($router);
 
-			$this->authorizedRoutes($router);
+            $this->authorizedRoutes($router);
 
-			$this->guestRoutes($router);
+            $this->guestRoutes($router);
 
-			$this->adminRoutes($router);
-		});
-	}
+            $this->adminRoutes($router);
+        });
+    }
 
-	/**
-	 * Public routes.
-	 *
-	 * @param Router $router
-	 */
-	protected function publicRoutes(Router $router)
-	{
-		// Homepage
-		$router->get('/', $this->cmsNamespace . 'HomeController@index');
+    /**
+     * Public routes.
+     *
+     * @param Router $router
+     */
+    protected function publicRoutes(Router $router)
+    {
+        // Homepage
+        $router->get('/', $this->cmsNamespace . 'HomeController@index');
 
-		// User email verification
-		$router->get('auth/register/verify/{token}', 'Auth\AuthController@verifyEmail');
-	}
+        // User email verification
+        $router->get('auth/register/verify/{token}', 'Auth\AuthController@verifyEmail');
+    }
 
-	/**
-	 * Authorized routes.
-	 *
-	 * @param Router $router
-	 */
-	protected function authorizedRoutes(Router $router)
-	{
-		$router->group([
-			'middleware' => IsAuthenticated::class,
-			'namespace' => 'Auth',
-			'prefix' => 'auth'
-		], function () {
-			// Authentication logout
-			$this->get('logout', 'AuthController@getLogout')->name('logout');
+    /**
+     * Authorized routes.
+     *
+     * @param Router $router
+     */
+    protected function authorizedRoutes(Router $router)
+    {
+        $router->group([
+            'middleware' => IsAuthenticated::class,
+            'namespace' => 'Auth',
+            'prefix' => 'auth'
+        ], function () {
+            // Authentication logout
+            $this->get('logout', 'AuthController@getLogout')->name('logout');
 
-			// User profile
-			$this->get('profile', 'ProfileController@index')->name('profile');
-			$this->post('profile/update', 'ProfileController@updateProfile')->name('profile_update');
-		});
-	}
+            // User profile
+            $this->get('profile', 'ProfileController@index')->name('profile');
+            $this->post('profile/update', 'ProfileController@updateProfile')->name('profile_update');
+        });
+    }
 
-	/**
-	 * Guest routes.
-	 *
-	 * @param Router $router
-	 */
-	protected function guestRoutes(Router $router)
-	{
-		$router->group([
-			'middleware' => IsGuest::class,
-			'namespace' => 'Auth',
-			'prefix' => 'auth'
-		], function () {
-			// Authentication login
-			$this->get('login', $this->authController . '@getLogin')->name('login');
-			$this->post('login', 'AuthController@postLogin')->name('login_action');
+    /**
+     * Guest routes.
+     *
+     * @param Router $router
+     */
+    protected function guestRoutes(Router $router)
+    {
+        $router->group([
+            'middleware' => IsGuest::class,
+            'namespace' => 'Auth',
+            'prefix' => 'auth'
+        ], function () {
+            // Authentication login
+            $this->get('login', $this->authController . '@getLogin')->name('login');
+            $this->post('login', 'AuthController@postLogin')->name('login_action');
 
-			// Registration
-			$this->get('register', 'AuthController@getRegister')->name('register');
-			$this->post('register', 'AuthController@postRegister')->name('register_action');
+            // Registration
+            $this->get('register', 'AuthController@getRegister')->name('register');
+            $this->post('register', 'AuthController@postRegister')->name('register_action');
 
-			// Social authentication
-			$this->get('social/{provider?}', 'SocialAuthController@getLogin')->name('login_social');
+            // Social authentication
+            $this->get('social/{provider?}', 'SocialAuthController@getLogin')->name('login_social');
 
-			// Password reset
-			$this->get('password/email', 'PasswordController@getEmail')->name('password_forget');
-			$this->post('password/email', 'PasswordController@postEmail')->name('password_send');
-			$this->get('password/reset/{token}', 'PasswordController@getPassword')->name('password_token');
-			$this->post('password/reset', 'PasswordController@postPassword')->name('password_reset');
-		});
-	}
+            // Password reset
+            $this->get('password/email', 'PasswordController@getEmail')->name('password_forget');
+            $this->post('password/email', 'PasswordController@postEmail')->name('password_send');
+            $this->get('password/reset/{token}', 'PasswordController@getPassword')->name('password_token');
+            $this->post('password/reset', 'PasswordController@postPassword')->name('password_reset');
+        });
+    }
 
-	/**
-	 * Admin routes.
-	 *
-	 * @param Router $router
-	 */
-	protected function adminRoutes(Router $router)
-	{
-		$router->group([
+    /**
+     * Admin routes.
+     *
+     * @param Router $router
+     */
+    protected function adminRoutes(Router $router)
+    {
+        $router->group([
 //			'middleware' => IsAdmin::class,
 //			'namespace' => $this->adminNamespace,
             'prefix' => 'admin'
