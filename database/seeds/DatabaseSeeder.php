2f95ac9de96e404ffc23713550d006ee4b9728a7
Blaž Oražem <blaz@orazem.info>
2015-12-16 23:50:54 +0100

    Working on seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 class DatabaseSeeder extends Seeder
 {
     /**
+     * Database tables.
+     *
+     * @var array
+     */
+    private $tables = [
+        'users',
+        'roles',
+        'permissions',
+        'permission_role',
+        'role_user',
+    ];
+
+    /**
      * Run the database seeds.
      *
      * @return void
@@ -14,8 +27,29 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->cleanDatabase();
+        $this->command->info('Database truncated.');
+
+        $this->call('UserTableSeeder::class');
+        $this->command->info('Users table seeded.');
+
         // $this->call(UserTableSeeder::class);
 
         Model::reguard();
+    }
+
+    /**
+     * Truncate tables.
+     */
+    public function cleanDatabase()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        foreach($this->tables as $tableName)
+        {
+            DB::table($tableName)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        $items = [
+            ['id' => '1', 'name' => 'John Doe', 'nickname' => 'JohnDoe', 'email' => 'john@numencode.com', 'password' =>'$2y$10$5QYFCfkd5lSOxX20i0w2p.hwSupOP.YcJxRRoL8073FzXQXADTDZy', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => '2', 'name' => 'Jane Doe', 'nickname' => 'JaneDoe', 'email' => 'jane@numencode.com', 'password' =>'$2y$10$5QYFCfkd5lSOxX20i0w2p.hwSupOP.YcJxRRoL8073FzXQXADTDZy', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+            ['id' => '3', 'name' => 'Jack Doe', 'nickname' => 'JackDoe', 'email' => 'jack@numencode.com', 'password' =>'$2y$10$5QYFCfkd5lSOxX20i0w2p.hwSupOP.YcJxRRoL8073FzXQXADTDZy', 'created_at' => new DateTime, 'updated_at' => new DateTime],
+        ];
+
+        DB::table('users')->insert($items);
+    }
+
+}
