a3ed93bbb71591a4e7e1028ba6ecaf6c14dc1ca4
Blaž Oražem <blaz.orazem@spletna-postaja.com>
2017-02-16 14:56:31 +0100

    Composer update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends ConsoleKernel
         \Numencode\Console\Commands\Npm::class,
         \Numencode\Console\Commands\TruncateTable::class,
         \Numencode\Console\Commands\Yarn::class,
-        \Spatie\MigrateFresh\Commands\MigrateFresh::class,
     ];
 
     /**
