1a075e0dd33c53541d88cb83cd723539534ed06d
Blaz Orazem <blaz@orazem.info>
2017-04-15 17:59:08 +0200

    Tree menu links

--- modules/Admin/Resources/views/menus/item.blade.php
+++ modules/Admin/Resources/views/menus/item.blade.php
@@ -1,5 +1,5 @@
 <li data-jstree='{"type":"{{ isset($menu[$item->id]) ? 'folder' : 'page' }}"}'>
-    <span title="{{ $item->title }}" onclick="javascript:location.href='#'">{{ $item->title }}</span>
+    <span title="{{ $item->title }}" onclick="javascript:location.href='{{ route('pages.edit', ['page' => $item]) }}'">{{ $item->title }}</span>
     @if(isset($menu[$item->id]))
         @include('admin::menus.list', ['collection' => $menu[$item->id]])
     @endif
